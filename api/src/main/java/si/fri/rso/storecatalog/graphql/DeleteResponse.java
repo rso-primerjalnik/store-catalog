f085da6371d29f304156ea35bb2942fb99db4f4d
Anže Oblak <anze.oblak@rettro.si>
2023-01-10 19:20:05 +0100

    graphQL queries added

---
+++ api/src/main/java/si/fri/rso/storecatalog/graphql/DeleteResponse.java
@@ -0,0 +1,18 @@
+package si.fri.rso.storecatalog.graphql;
+
+public class DeleteResponse {
+
+    private boolean deleted;
+
+    public DeleteResponse(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
---
+++ api/src/main/java/si/fri/rso/storecatalog/graphql/StoreQueries.java
@@ -0,0 +1,54 @@
+package si.fri.rso.storecatalog.graphql;
+
+import com.kumuluz.ee.graphql.annotations.GraphQLClass;
+import com.kumuluz.ee.graphql.classes.Filter;
+import com.kumuluz.ee.graphql.classes.Pagination;
+import com.kumuluz.ee.graphql.classes.PaginationWrapper;
+import com.kumuluz.ee.graphql.classes.Sort;
+import com.kumuluz.ee.graphql.utils.GraphQLUtils;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import si.fri.rso.storecatalog.lib.Store;
+import si.fri.rso.storecatalog.services.beans.StoreBean;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@GraphQLClass
+@ApplicationScoped
+public class StoreQueries {
+
+    @Inject
+    private StoreBean storeBean;
+
+    @GraphQLMutation
+    public Store addStore(@GraphQLArgument(name = "store") Store store) {
+        return storeBean.createStore(store);
+    }
+
+    @GraphQLMutation
+    public Store editStore(@GraphQLArgument(name = "storeId") Integer id,
+                           @GraphQLArgument(name = "store") Store store) {
+        return storeBean.putStore(id, store);
+    }
+
+    @GraphQLMutation
+    public DeleteResponse deleteStore(@GraphQLArgument(name = "storeId") Integer id) {
+        return new DeleteResponse(storeBean.deleteStore(id));
+    }
+
+    @GraphQLQuery
+    public PaginationWrapper<Store> getAllStores (@GraphQLArgument(name = "pagination") Pagination pagination,
+                                               @GraphQLArgument(name = "sort") Sort sort,
+                                               @GraphQLArgument(name = "filter") Filter filter) {
+
+        return GraphQLUtils.process(storeBean.getStores(), pagination, sort, filter);
+    }
+
+    @GraphQLQuery
+    public Store getSingleStore(@GraphQLArgument(name = "id") Integer id) {
+        return storeBean.getStoreById(id);
+    }
+
+}
